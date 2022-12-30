a595e892bebad9e7bd33dc28f7d89b0420d44602
Prince Chaurasia <113881642+PrinceChaursia@users.noreply.github.com>
2022-12-30 13:28:30 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String [] args) {
+
+    System.out.println("(1)=> Hello everyone. This is PRINCE CHAURASIA.");
+        System.out.println("(2)=> from GHAZIPUR,PURWANCHAL(UTTAR PRADESH");
+        
+    }
+}
